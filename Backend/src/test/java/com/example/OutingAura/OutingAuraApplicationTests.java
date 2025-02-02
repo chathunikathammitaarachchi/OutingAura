dd8eebfc04603d000cf56ffdf9e0c8908092c57b
chathunikathammitaarachchi <chathunikathammitaarachchi74@gmail.com>
2025-02-02 20:00:08 +0530

    add

---
+++ Backend/src/test/java/com/example/OutingAura/OutingAuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OutingAura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutingAuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
